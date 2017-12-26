54ed11a24511de13a78d4458cd57f0224c4e9d5c
dwyn <dwayne.harmon@gmail.com>
2017-12-26 00:04:48 -0600

    Add more functionality for project controller.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,25 @@
 class ProjectsController < ApplicationController
 
   def index
-
+    @projects = Project.all.order("created_at desc")
   end
 
+  def new
+    @project = Project.new project_params
+  end
+
+  def create
+    @project = Project.new project_params
+
+    if @project.save
+      redirect_to @project, notice: "Nice! Project saved!"
+    else
+      render 'new'
+    end    
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :description, :lnk)
+  end
 end
